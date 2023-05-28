befe5d9b64ae0e10278e8a6244e93131e8fa5d86
Rovs26 <102684013+Rovs26@users.noreply.github.com>
2023-05-29 01:32:18 +0800

    Add files via upload

---
+++ SampleForBasicPython.py
@@ -0,0 +1,160 @@
+#Hello! Welcome to Intro Course in Python
+
+#Today we are going to learn assigning values in variables, do basic operations 
+# and learn how you can use Python in solving mathematical problems
+
+#Learning How to assign Values in Variables
+
+#If we want to assign a value to a variable, we first put the variable then an equal sign "=" then the corresponding value
+#An example would be "x = 3", now try doing it by yourself
+
+x = 3
+
+#Then we print the variable "x" to see the value of "x"
+
+print(x)
+
+#Now learning that you can assign integer values, you can also assign variables as values to an another variable
+#Here's an example "six = x * 2" (The * symbol represents multiplication)
+
+six = x * 2
+
+#The we print to show the value
+
+print(six)
+
+#Now that you understand the basic concept of assigning values to variables
+#Can you solve this problem?
+
+#INSTRUCTION: you have to exchange the "?" with the correct value to make it equal
+#Tip: "==" means equal to
+
+y = 8
+
+Test1 = 7 * (y + 3) == 77
+
+print(Test1)
+
+#If result states True, then answer is correct, If False then answer is wrong
+
+#---------------------------------------------------------------------------------------------------------------------------#
+
+#Now, Moving on to Basic Operations
+# "+" is for addition
+# "-" is for subtraction
+# "*" is for multiplication
+# "/" is for division
+
+#other complex operations are:
+# "**" is for Exponent
+# "//" is for floor division (Automatically Rounds down the result, if decimal is available)
+# "%" is for Modulo (Remainder of the division)
+# "==" is for equal
+# ">" is for greater than
+# "< is" for less than
+# ">=" is for greater than or equal
+# "<=" is for less than or equal
+
+#Now let us practice the operations
+
+#We will use "m" and "n" for the example
+
+m = 5
+n = 3
+
+#For Addition "+"
+
+Add = m + n
+print(Add)
+
+#For Subtraction "-"
+
+Sub = m - n
+print(Sub)
+
+#For Multiplication "*"
+
+Mul = m * n
+print(Mul)
+
+#For Division "/"
+
+Div = m / n
+print(Div)
+
+#For Exponent "**"
+Exp = m ** n
+print(Exp)
+
+#For Floor Division "//"
+F_Div = m // n
+print(F_Div)
+
+#For Modulo "%"
+Mod = m % n
+print(Mod)
+
+# Above represents how you can use the operations
+
+#---------------------------------------------------------------------------------------------------------------------------#
+
+#QUIZ!!!
+
+#1. Set "6" with  variable "x1", "13" with "y1" and "7" with "z1"
+    #Using the variable given, add 6 with 13 then subtract 7; name the operations "num_1"
+
+
+
+#2. What is the remainder of 3872 divided by 43, name the operation "num_2"
+
+
+
+#3. What is the remainder and quotient for "x3" raise to "y3" divided by "z3"
+    #If "x3 = 13", "y3 = 4", "z3 = 23". Name the operation "num_3Q" for the quotient and "num_3R" for the remainder
+
+
+
+#4. Find the value of "x4"
+x4 = n
+
+num_4 = 3 < x4 < 5
+print(num_4)
+
+#5. Divide "x5" with "y5"
+    #"x5 = 2/3", "y5 = 4/7". Assign name "num_5"
+
+
+
+#6. Input the correct operation between "(4 / 2)" and "8"
+
+num_6 = (4 / 2) + 8 + 23 == 39
+print(num_6)
+
+#7. What is the quotient when divided by "z7" of the remainder of "x7" and "y7"
+    #If "x7 = 9866", "y7 = 17", "z7 = 2". Assign name as "num_7"
+
+
+
+#8. Assign the correct values
+    # Given Values to use "4, 30, 83, 17". Only include the integer, without the decimal
+
+x8 = 17
+y8 = 4
+z8 = 83
+w8 = 30
+
+num_8 = (x8 ** y8) / z8 + w8 == 1036
+print(num_8)
+
+#9. Fulfill the arithmetic sequence using "x9 as number of times", "y9 as starting value" and "z9 as the difference for each sequence"
+    #Given that "x9 = 13" and "y9 = 7"
+
+x9 = 13
+y9 = 7
+z9 = n
+
+num_9 = ((x9 - 1) * z9) + y9 == 61
+print(num_9)
+
+#10. Create your own arithmetic sequence. Find the answer
+    #Given that "x10 = 7", "y10 = 6" and "z10 = 14". Name the operation "num_10"
